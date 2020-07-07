61be6dcdb57c62c2738c51e9bb2c5526d1a05515
Maxim <maximmalofeev@bk.ru>
2020-07-07 11:51:08 +0300

    added instal conan step

--- test.cpp
+++ test.cpp
@@ -1,11 +1,3 @@
 #include <iostream>
 
-int main(int argc, char const *argv[]) {
-#ifdef __linux__
-  std::cerr << "Unix test fail" << std::endl;
-  return 1;
-#elif WIN32
-  std::cout << "Win test ok" << std::endl;
-  return 0;
-#endif
-}
+int main(int argc, char const *argv[]) { return 0; }
